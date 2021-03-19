f2c3f65a0f088c5db6f866e9ecb8703d7e66826a
aolikas <46649354+aolikas@users.noreply.github.com>
2021-03-19 19:03:42 +0100

    added a new method rollDice()

--- app/src/main/java/my/e/dice/MainActivity.kt
+++ app/src/main/java/my/e/dice/MainActivity.kt
@@ -4,19 +4,38 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
+import android.widget.ImageView
 import android.widget.TextView
 import android.widget.Toast
 import org.w3c.dom.Text
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var ivDiceResult: ImageView
+    lateinit var btnRoll: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val btnRoll: Button = findViewById(R.id.btn_roll)
-        val txtResult: TextView = findViewById(R.id.txt_result)
+        btnRoll = findViewById(R.id.btn_roll)
+        ivDiceResult = findViewById(R.id.iv_result)
         btnRoll.setOnClickListener(View.OnClickListener {
-            Toast.makeText(this, "clicked", Toast.LENGTH_SHORT).show()
+            rollDice()
         })
+    }
 
+    private fun rollDice() {
+        val randomInt = Random().nextInt(6) + 1
+        val imgDice = when(randomInt) {
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
+        }
+
+        ivDiceResult.setImageResource(imgDice)
     }
 }
